a56eb006774195d1d742179e96d68408b801e735
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2023-07-15 15:29:51 -0500

    Added Binary Compatibility Plugin and Gradle Wrapper Validation Github Action

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     id("com.android.library") version "7.3.1" apply false
     id("com.android.application") version "7.3.1" apply false
     id("org.jetbrains.dokka") version "1.8.10"
+    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.13.2"
 }
 
 allprojects {
